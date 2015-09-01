35fcccdb508842885a76a45b923c1c9c9a9b1baf
Anagha Joshi <anagha.elex@gmail.com>
2015-09-01 01:49:08 -0400

    Add name and role using prepend instead of append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,10 +3,8 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 var formattedName = HTMLheaderName.replace("%data%","Anagha Joshi");
-$("#header").append(formattedName);
-
-
 var formattedRole = HTMLheaderRole.replace("%data%","Computer Engineering Graduate Student");
-$("#header").append(formattedRole);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
 console.log("success!")
