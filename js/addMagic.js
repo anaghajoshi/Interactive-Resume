9bd2010d325691c7bb1801e4d9cf34322730bb49
Anagha Joshi <anagha.elex@gmail.com>
2015-09-22 20:08:14 -0400

    Delete Phone number and Update Contact to Contact Form

--- js/addMagic.js
+++ js/addMagic.js
@@ -4,7 +4,7 @@
 function contactFn(){
     document.getElementById('chart-div').style.display='inline';
     document.getElementById('chart-div')
-        .innerHTML =' <div> <object type="text/html" data="./contact.html" target="_top" width="500px" height="700px" style="overflow:auto"> </object></div>'
+        .innerHTML =' <div> <object type="text/html" data="./contactForm.html" target="_top" width="500px" height="700px" style="overflow:auto"> </object></div>'
 
 
 }
